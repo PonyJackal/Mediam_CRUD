c9d5e20059a0bb73ca4cd0d7ca9210aed28cf191
PonyJackal <pony1125@outlook.com>
2022-10-03 06:08:56 -0700

    feat: add user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ArticlesModule } from './articles/articles.module';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [PrismaModule, ArticlesModule, AuthModule],
+  imports: [PrismaModule, ArticlesModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
