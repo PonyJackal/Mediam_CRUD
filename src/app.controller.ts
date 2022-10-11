8679c6b00a901c7ab8877122b709692300123d81
PonyJackal <pony1125@outlook.com>
2022-10-10 20:18:21 -0700

    feat: add public decorator

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,6 +3,7 @@ import { AppService } from './app.service';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 import { AuthService } from './auth/auth.service';
 import { UsersService } from './users/users.service';
+import { Public } from './app.decorator';
 
 @Controller()
 export class AppController {
@@ -18,9 +19,9 @@ export class AppController {
     return this.authService.login(req.user);
   }
 
+  @Public()
   @Post('auth/register')
   async register(@Request() req) {
-    console.log("req", req.body)
     return this.usersService.create(req.body);
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,22 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ArticlesModule } from './articles/articles.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
 @Module({
   imports: [PrismaModule, ArticlesModule, AuthModule, UsersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    // {
+    //   provide: APP_GUARD,
+    //   useClass: JwtAuthGuard
+    // }
+  ],
 })
 export class AppModule {}
